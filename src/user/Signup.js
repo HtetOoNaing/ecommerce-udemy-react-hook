452567d56932f0b628e2d1fe7593601abb3ba2d1
HtetOoNaing <htetoonaing.ycc@gmail.com>
2021-07-05 00:59:21 +0630

    add environment variables

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { API } from '../config';
 import Layout from '../core/Layout';
 
 const Signup = () => (
 	<Layout title="Signup" description="Signup to Node React E-commerce App">
-		...
+		{API}
 	</Layout>
 );
  
