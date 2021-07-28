ca61fe3885a866c608e8d5f88b448507996175d2
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-07-28 11:06:44 +0630

    cart item total in menu

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from '../auth';
+import { itemTotal } from './cartHelpers';
 
 const isActive = (history, path) => {
 	if (history.location.pathname === path) {
@@ -18,6 +19,9 @@ const Menu = ({ history }) => (
 			</li>
 			<li className="nav-item">
 				<Link to="/shop" className="nav-link" style={isActive(history, '/shop')} >Shop</Link>
+			</li>
+			<li className="nav-item">
+				<Link to="/cart" className="nav-link" style={isActive(history, '/cart')} >Cart <sup><small className="cart-badge">{itemTotal()}</small></sup> </Link>
 			</li>
 			{isAuthenticated() && isAuthenticated().user.role === 0 && (
 				<li className="nav-item">
